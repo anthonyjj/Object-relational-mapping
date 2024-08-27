514a075b0ad603f1234cf9cedde710b283eda67c
anthonyjj <156860966+anthonyjj@users.noreply.github.com>
2024-08-26 23:39:34 -0700

    defined columns in product.js

--- models/Product.js
+++ models/Product.js
@@ -10,14 +10,38 @@ class Product extends Model {}
 Product.init(
   {
     // define columns
-    product_name: {
-      type: DataTypes.STRING, allowNull: false,
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
     },
-
-  price: {
-    type: DataTypes.DECIMAL, allowNull: false,
-    validate: {
-      isDecimal: true,
+    product_name: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    price: {
+      type: DataTypes.DECIMAL(10, 2),
+      allowNull: false,
+      validate: {
+        isDecimal: true
+      }
+    },
+    stock: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      defaultValue: 10,
+      validate: {
+        isNumeric: true
+      }
+    },
+    category_id: {
+      type: DataTypes.INTEGER,
+      // References the `Category` model's `id`.
+      references: {
+        model: 'category',
+        key: 'id'
+      }
     }
   },
   {
@@ -27,7 +51,6 @@ Product.init(
     underscored: true,
     modelName: 'product',
   }
-},
 );
 
 module.exports = Product;
